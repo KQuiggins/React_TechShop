65881d4a563d8d083bbbd2e472c543f220eb6b5c
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-06-23 15:17:56 -0400

    added products context

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Userprovider } from './context/user.context';
+import {ProductsProvider} from './context/products.context';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Userprovider>
+        <ProductsProvider>
         <App />
+        </ProductsProvider>
       </Userprovider>
     </BrowserRouter>
   </React.StrictMode>,
