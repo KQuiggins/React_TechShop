a84011918dd9918a59beadbabc82e7934e6f7ba7
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-06-27 15:30:24 -0400

    finished product card

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -6,7 +6,8 @@ const ProductCard = ({ product }) => {
 
     const { name, price, imageUrl } = product;
 
-   return ( <div>
+   return ( 
+   <div className="product-card-container">
         <img src={imageUrl} alt={`${name}`} />
         <div className="footer">
             <span className="name">{name}</span>
