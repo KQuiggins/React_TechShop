dbcc6bf0bca035f895a2dca991b982d8eed15da7
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-07-01 16:13:21 -0400

    worked on adding items to cart and quantities

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,14 @@
 import Button from '../button/button.component';
 
+
 import './cart-dropdown.styles.scss';
+import CartItem from './../cart-item/cart-item.component.jsx';
 
 const CartDropdown = () => (
   <div className='cart-dropdown-container'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {[].map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+    </div>
     <Button>GO TO CHECKOUT</Button>
   </div>
 );
